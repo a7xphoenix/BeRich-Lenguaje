9b9174914fa13ee89bbe5785642e481ae668bd08
a7xphoenix <a7xphoenix@hotmail.com>
2015-11-21 19:41:12 -0600

    solo falta validar comentarios

---
+++ AnalizadorLexico/matriztran/Form1.Designer.cs
@@ -0,0 +1,89 @@
+﻿namespace matriztran
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnValidar = new System.Windows.Forms.Button();
+            this.txtId = new System.Windows.Forms.TextBox();
+            this.txtTokens = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // btnValidar
+            // 
+            this.btnValidar.Location = new System.Drawing.Point(350, 10);
+            this.btnValidar.Name = "btnValidar";
+            this.btnValidar.Size = new System.Drawing.Size(80, 25);
+            this.btnValidar.TabIndex = 6;
+            this.btnValidar.Text = "▶  &Compilar";
+            this.btnValidar.UseVisualStyleBackColor = true;
+            this.btnValidar.Click += new System.EventHandler(this.btnValidar_Click);
+            // 
+            // txtId
+            // 
+            this.txtId.Location = new System.Drawing.Point(12, 41);
+            this.txtId.Multiline = true;
+            this.txtId.Name = "txtId";
+            this.txtId.Size = new System.Drawing.Size(374, 443);
+            this.txtId.TabIndex = 4;
+            // 
+            // txtTokens
+            // 
+            this.txtTokens.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtTokens.Location = new System.Drawing.Point(392, 41);
+            this.txtTokens.Multiline = true;
+            this.txtTokens.Name = "txtTokens";
+            this.txtTokens.ReadOnly = true;
+            this.txtTokens.Size = new System.Drawing.Size(374, 443);
+            this.txtTokens.TabIndex = 10;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(778, 496);
+            this.Controls.Add(this.txtTokens);
+            this.Controls.Add(this.btnValidar);
+            this.Controls.Add(this.txtId);
+            this.MinimumSize = new System.Drawing.Size(794, 535);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.SizeChanged += new System.EventHandler(this.Form1_Resize);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button btnValidar;
+        private System.Windows.Forms.TextBox txtId;
+        private System.Windows.Forms.TextBox txtTokens;
+    }
+}
+
---
+++ AnalizadorLexico/matriztran/Form1.cs
@@ -0,0 +1,260 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.OleDb;
+using System.IO;
+using System.Diagnostics;
+
+namespace matriztran
+{
+    public partial class Form1 : Form
+    {
+        List<string> columnas = new List<string>();
+
+        public Form1()
+        {
+            InitializeComponent();
+            RecargarMatriz();
+            LeerColumnas();
+        }
+
+        private void LeerColumnas()
+        {
+            for (int i = 1; i < DtSet.Tables[0].Columns.Count-1; i++)
+            {
+                string aux = DtSet.Tables[0].Columns[i].Caption.ToString();
+
+                if (aux == "PUNTO")
+                {
+                    columnas.Add(".");
+                }
+                else if (aux == "NOT")
+                {
+                    columnas.Add("!");
+                }
+                else if (aux == "COR_A")
+                {
+                    columnas.Add("[");
+                }
+                else if (aux == "COR_C")
+                {
+                    columnas.Add("]");
+                }
+                else if (aux == " ")
+                {
+                    columnas.Add("⇢");
+                }
+                else if (aux.Contains("1") && aux != "1")
+                {
+                    aux = aux.Substring(0,1);
+                    columnas.Add(aux);
+                }
+                else
+                {
+                    columnas.Add(aux);
+                }
+            }
+        }
+
+        DataSet DtSet;
+        private void RecargarMatriz()
+        {
+            OleDbConnection MyConnection;
+            OleDbDataAdapter MyCommand;
+            MyConnection = new OleDbConnection(@"provider=Microsoft.ACE.OLEDB.12.0;Data Source='C:/Datos/lenguaje.xlsx';Extended Properties=Excel 12.0;");
+            MyCommand = new OleDbDataAdapter("select * from [Hoja1$]", MyConnection);
+
+            MyCommand.TableMappings.Add("Table", "Table");
+            DtSet = new DataSet();
+            DtSet.CaseSensitive = true;
+            
+            MyCommand.Fill(DtSet);
+            MyConnection.Close();
+        }
+
+        string id;
+        int indice = 0;
+        int Estado = 0;
+        bool DEL = false;
+        int ContIds = 0;
+        private void btnValidar_Click(object sender, EventArgs e)
+        {
+            RecargarMatriz();
+            id = txtId.Text;
+            id = id.Replace(Environment.NewLine, " ");
+            indice = 0;
+            Estado = 0;
+            DEL = false;
+            txtTokens.Text = "";
+            ContIds = 0;
+            bool BanCadena = false;
+
+            ///////////////
+            try
+            {
+                while (indice <= id.Length)
+                {
+                    char Actual = '␀';
+
+                    if (indice == id.Length)
+                        DEL = true;
+                    else
+                    {
+                        Actual = id[indice];
+                        if (Actual == '"' && !BanCadena)
+                            BanCadena = true;
+                        else if (Actual == '"' && BanCadena)
+                            BanCadena = false;
+
+                        if (Actual == ' ' && BanCadena)
+                            Actual = '⇢';
+                        else if (Actual == ' ' && !BanCadena)
+                            DEL = true;
+                    }
+                    bool bandera = false;
+
+                    for (int i = 0; i < columnas.Count; i++)
+                    {
+                        string celda = DtSet.Tables[0].Rows[Estado][i + 1].ToString();
+                        if (DEL)
+                        {
+                            if (columnas[i] == "DEL")
+                            {
+                                Estado = int.Parse(celda);
+                                Estado--;
+                                celda = DtSet.Tables[0].Rows[Estado][i + 1].ToString();
+
+                                if (celda.Contains("ACEPTA"))
+                                {
+                                    string token = DtSet.Tables[0].Rows[Estado][i + 2].ToString();
+                                    txtTokens.Text += token;
+                                    if (token == "ID")
+                                        txtTokens.Text += ++ContIds;
+                                    txtTokens.Text += " ";
+                                    bandera = true;
+                                    Estado = 0;
+                                    DEL = false;
+                                }
+                                else if (celda != "ERROR" && celda != "")
+                                {
+                                    Estado = int.Parse(celda);
+                                    Estado--;
+                                    bandera = true;
+                                }
+                            }
+                        }
+                        else if (Actual.ToString() == columnas[i])
+                        {
+                            MessageBox.Show(Actual.ToString() + " " + celda);
+                            if (celda != "ERROR" && celda != "")
+                            {
+                                Estado = int.Parse(celda);
+                                Estado--;
+                                bandera = true;
+                            }
+                            else if (celda == "ERROR")
+                            {
+                                throw new Exception("Error de léxico");
+                                
+                            }
+                        }
+                    }
+                    if (!bandera)
+                    {
+                        throw new Exception("Error de léxico");
+                    }
+                    indice++;
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+                return;
+            }
+            Acomodar();
+        }
+
+        private bool Contains(char c, char[] arr)
+        {
+            for (int i = 0; i < arr.Length; i++)
+            {
+                if (c == arr[i])
+                    return true;
+            }
+            return false;
+        }
+
+        private void Acomodar()
+        {
+            string fuente = txtId.Text;
+            string destino = txtTokens.Text;
+            fuente = fuente.Replace("\r\n", "▾");
+            List<int> espacios = new List<int>();
+
+            int cont = 0;
+            foreach (char x in fuente)
+            {
+                if(x == ' ')
+                    cont++;
+                if (x == '▾')
+                {
+                    cont++;
+                    espacios.Add(cont);
+                }
+            }
+
+            cont = 0;
+            txtTokens.Text = "";
+            for (int i = 0; i < destino.Length; i++)
+            {
+                if (destino[i] == ' ')
+                {
+                    cont++;
+                    if (espacios.Contains(cont))
+                        txtTokens.Text += Environment.NewLine;
+                    else
+                        txtTokens.Text += destino[i];
+                }
+                else
+                    txtTokens.Text += destino[i];
+            }
+
+            string Ruta = @"C:\datos\";
+            if (!Directory.Exists(Ruta))
+                Directory.CreateDirectory(Ruta);
+            string archivo = DateTime.Now + ".txt";
+            archivo = archivo.Replace('/', '.');
+            archivo = archivo.Replace(':','.');
+            Ruta += archivo;
+            //FileStream FS = new FileStream(Ruta, FileMode.Create,FileAccess.Write);
+            StreamWriter SW = new StreamWriter(Ruta, true, Encoding.UTF8);
+            foreach (char c in txtTokens.Text)
+            {
+                SW.Write(c);
+            }
+            SW.Close();
+            Process.Start(Ruta);
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void Form1_Resize(object sender, EventArgs e)
+        {
+            int width = (this.Size.Width - 46) / 2;
+            int height = (this.Size.Height - 92);
+            this.txtId.Size = new Size(width, height);
+            this.txtTokens.Size = new Size(width, height);
+            this.txtTokens.Location = new Point(width + 20, 41);
+            btnValidar.Location = new Point((this.Size.Width - 90) / 2, 10);
+        }
+    }
+}
